270580c33730942410874225f859cfe3f495aaef
prashu18400 <prashanth18400@gmail.com>
2020-09-08 09:47:11 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component, VERSION } from '@angular/core';
-
+import {Hero} from './hero';
 @Component({
   selector: 'my-app',
+  template : `
+  <h1>My name is Prashanth</h1>`,
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  title = 'Tour of Heroes';
+  prducts = ['Shirt','Trousers','Watches'];
+  heroos=[
+    new Hero(1,"Superman"),
+    new Hero(2,"Batman")
+  ]
 }
